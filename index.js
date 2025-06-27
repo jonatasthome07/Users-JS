5f1f90688b1c38a154eb109d04fff51a20f8553b
Jônatas <jonatasthome04@gmail.com>
2025-06-27 18:54:41 -0300

    Added middlewares and engine

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const conn = require("./db/conn")
 const User = require("./models/User")
 const exphbs = require("express-handlebars")
 
+app.engine("handlebars", exphbs.engine())
+app.set("view engine", "handlebars")
+
+app.use(express.static("public"))
+
+app.get("/", async(req,res)=>{
+    res.render("home")
+})
 
 conn.sync()
 .then(()=>{
