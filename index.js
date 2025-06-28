6adbd2548eea78a6ead4c225504c0b641dd50c77
Jônatas <jonatasthome04@gmail.com>
2025-06-27 22:28:10 -0300

    Form to add users in database

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(express.static("public"))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
+app.get("/adduser", async (req,res)=>{
+    res.render("adduser")
+})
+
 app.get("/", async(req,res)=>{
     res.render("home")
 })
