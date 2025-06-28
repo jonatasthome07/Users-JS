9b3ae2c7928bd79fa81c8f00a0d91e41520e1b12
Jônatas <jonatasthome04@gmail.com>
2025-06-27 22:23:59 -0300

    New model added

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const app = express();
 const conn = require("./db/conn")
 const User = require("./models/User")
 const exphbs = require("express-handlebars")
+const Address = require("./models/Address")
 
 app.engine("handlebars", exphbs.engine())
 app.set("view engine", "handlebars")
 
 app.use(express.static("public"))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 app.get("/", async(req,res)=>{
     res.render("home")
--- models/User.js
+++ models/User.js
@@ -12,4 +12,5 @@ const User = db.define("User", {
     }
 })
 
+
 module.exports = User;
